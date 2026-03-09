a83bbe32aa66c55acb261d7c726f4bf0d43996c8
VojtGar <memroveccz@gmail.com>
2026-03-09 13:53:25 +0100

    Add files via upload

---
+++ 9.3.2026/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace _9._3._2026
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
